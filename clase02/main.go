271a4ad7ebd42446de911aeb4e9db5e72c729029
ValentinZoia <valentinzoia@gmail.com>
2024-06-25 17:24:04 -0300

    Agregando archivo sobre gorutines

--- clase02/main.go
+++ clase02/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Println("hi")
+	/*
+		-----------------------GORUTINES------------------------
+	*/
+
+	//crear canal
+	c := make(chan int)
+
+	//disparar gorutine. no olvidar palabra reservada go
+	go doSomething(c)
+	<-c // // Recibir el valor del canal
+}
+
+// Creamos una funcion que simule una gorutine
+func doSomething(c chan int) {
+	time.Sleep(3 * time.Second)
+	fmt.Println("Done")
+	c <- 1
 }
