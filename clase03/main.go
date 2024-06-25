f49af7ffbb2ce146991b611f943b77abd6bf186c
ValentinZoia <valentinzoia@gmail.com>
2024-06-25 20:12:59 -0300

    Modificacion de archivo sobre punteros

--- clase03/main.go
+++ clase03/main.go
@@ -1,8 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 	/*
 		-----------APUNTADORES/PUNTEROS-----------
 	*/
-
+	g := 25
+	fmt.Println(g)
+	h := &g        // h es un apuntador de g. El valor de h es la direccion de memoria de g
+	fmt.Println(h) //0xc00000a0b8
+	//El operador * indica a que valor apunta esa direccion de memoria
+	fmt.Println(*h) //25
 }
